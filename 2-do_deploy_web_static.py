2360072d79fb4ab1ed72ad2ae22618163db54787
sazad44 <530@holbertonschool.com>
2019-04-09 00:13:00 +0000

    Uploading task 2 fabfile to deploy web_static folder to server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Fab file to archive web_static content"""
+from os.path import exists
+from fabric.api import *
+from datetime import datetime
+
+
+env.hosts = ['172.31.54.208']
+env.user = 'root'
+
+
+def do_pack():
+    """Pack web_static files into archive"""
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions/")
+    path = local("tar -cvzf versions/web_static_{}.tgz web_static".format(dt))
+    return path
+
+
+def do_deploy(archive_path):
+    """Deploy function for archive to get deployed to servers"""
+    if not exists(archive_path):
+        return False
+    fileNameExt = archive_path.split('/')[1]
+    fileName = fileNameExt.split(".")[0]
+    result = put(archive_path, '/tmp/{}'.format(fileNameExt))
+    if result.failed:
+        return False
+    result = run("mkdir -p /data/web_static/releases/{}/".format(fileName))
+    if result.failed:
+        return False
+    with cd('/tmp'):
+        result = run("tar -xzf {} -C /data/web_static/releases/{}/"
+                     .format(fileNameExt, fileName))
+        if result.failed:
+            return False
+    result = run("rm /tmp/{}".format(fileNameExt))
+    if result.failed:
+        return False
+    input = "mv -f -t /data/web_static/releases/{}/ /data/web_static/releases/{}/web_static/*".format(fileName, fileName)
+    result = sudo(input)
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/releases/{}/web_static"
+                 .format(fileName))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/current")
+    if result.failed:
+        return False
+    result = run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"
+        .format(fileName))
+    if result.failed:
+        return False
+    print("New Version Deployed!")
+    return True
