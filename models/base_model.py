b0cb089135eaac51a0261c32cedc6d4786fb03e8
sazad44 <530@holbertonschool.com>
2019-04-24 15:45:28 +0000

    Updating base model class to keep id and created at constant even with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,17 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if 'id' not in self.__dict__.keys():
+                self.id = str(uuid.uuid4())
+            if 'created at' not in self.__dict__.keys():
+                self.created_at = datetime.now()
+            if 'updated at' not in self.__dict__.keys():
+                self.updated_at = datetime.now()
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
